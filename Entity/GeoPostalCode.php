9e64ede0a4c850b09d2f5a51f4c2ab70babf9b6b
Ray <ray@discountofficeitems.com>
2012-06-27 15:30:47 -0500

    Added distance by KM

--- Entity/GeoPostalCode.php
+++ Entity/GeoPostalCode.php
@@ -92,6 +92,19 @@ class GeoPostalCode {
 	}
 
 	/**
+	 * Get the distance in Km between two GeoPostalCodes
+	 *
+	 * Roughly based on notes from query based version and
+	 * http://en.wikipedia.org/wiki/Great-circle_distance
+	 *
+	 * @return float
+	 */
+	public function getKmTo(GeoPostalCode $other) {
+		// ~6371 = Mean radius of Earth in KM
+		return 6371 * $this->getDeltaAngleTo($other);
+	}
+
+	/**
 	 * Get the distance in miles between two GeoPostalCodes
 	 *
 	 * Roughly based on notes from query based version and
@@ -100,13 +113,26 @@ class GeoPostalCode {
 	 * @return float
 	 */
 	public function getMilesTo(GeoPostalCode $other) {
+		// ~3959 = Mean radius of Earth in Miles
+		return 3959 * $this->getDeltaAngleTo($other);
+	}
+
+	/**
+	 * Get the distance in miles between two GeoPostalCodes
+	 *
+	 * Roughly based on notes from query based version and
+	 * http://en.wikipedia.org/wiki/Great-circle_distance
+	 *
+	 * @return float
+	 */
+	protected function getDeltaAngleTo(GeoPostalCode $other) {
 		// Convert to radians
 		$deltaLat = deg2rad($this->getLat() - $other->getLat());
 		$deltaLng = deg2rad($this->getLng() - $other->getLng());
 		$origLat  = deg2rad($this->getLat());
 		$destLat  = deg2rad($other->getLat());
 
-		return 3956 * 2 * asin(
+		return 2 * asin(
 			sqrt(
 				pow(sin($deltaLat / 2), 2) +
 				cos($origLat) *
--- Tests/Entity/GeoPostalCodeTest.php
+++ Tests/Entity/GeoPostalCodeTest.php
@@ -20,6 +20,21 @@ class GeoPostalCodeTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals($expected, $orig->getMilesTo($dest), '', 0.5);
 	}
 
+	/**
+	 * @dataProvider sampleDistances
+	 */
+	public function testGetKmTo($origLat, $origLng, $destLat, $destLng, $expected) {
+		$orig = new GeoPostalCode();
+		$orig->setLat($origLat);
+		$orig->setLng($origLng);
+
+		$dest = new GeoPostalCode();
+		$dest->setLat($destLat);
+		$dest->setLng($destLng);
+
+		$this->assertEquals($expected * 1.609344, $orig->getKmTo($dest), '', 0.5);
+	}
+
 	public function sampleDistances() {
 		return array(
 			array(32.9697, -96.8032, 29.4678, -98.5350, 262.67779380543),
