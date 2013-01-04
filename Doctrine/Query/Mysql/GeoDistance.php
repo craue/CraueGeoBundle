4194d08661f2b02cdf80075faa39a780b39fa531
Christian Raue <christian.raue@gmail.com>
2013-01-04 16:54:52 +0100

    updated copyright year range

--- Doctrine/Query/Mysql/GeoDistance.php
+++ Doctrine/Query/Mysql/GeoDistance.php
@@ -12,7 +12,7 @@ use Doctrine\ORM\Query\SqlWalker;
  * Returns: distance in km
  *
  * @author Christian Raue <christian.raue@gmail.com>
- * @copyright 2011-2012 Christian Raue
+ * @copyright 2011-2013 Christian Raue
  * @license http://www.opensource.org/licenses/mit-license.php MIT License
  */
 class GeoDistance extends FunctionNode {
