22c13c3f2c9573ce846f858e7dce3da6ef0a732f
Christian Raue <christian.raue@gmail.com>
2015-02-23 16:32:16 +0100

    updated Travis config

--- travis_install_dependencies.sh
+++ travis_install_dependencies.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+export COMPOSER_NO_INTERACTION=1
+composer self-update
 composer config -g preferred-install source
 
 if [ -n "${MIN_STABILITY:-}" ]; then
 	sed -i -e "s/\"minimum-stability\": \"stable\"/\"minimum-stability\": \"${MIN_STABILITY}\"/" composer.json
 fi
 
-composer --no-interaction require --no-update symfony/framework-bundle:${SYMFONY_VERSION}
-composer --no-interaction require --no-update --dev symfony/symfony:${SYMFONY_VERSION}
-composer --no-interaction update
+composer remove --no-update symfony/framework-bundle
+composer require --no-update --dev symfony/symfony:${SYMFONY_VERSION}
+composer update
