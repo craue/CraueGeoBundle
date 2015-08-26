2c10296be85dd74bc40169f8c422aaf8a14c4b3c
Christian Raue <christian.raue@gmail.com>
2015-08-26 12:33:42 +0200

    added more tests

--- Tests/Doctrine/Query/Mysql/GeoDistanceTest.php
+++ Tests/Doctrine/Query/Mysql/GeoDistanceTest.php
@@ -14,12 +14,23 @@ use Craue\GeoBundle\Tests\IntegrationTestCase;
 class GeoDistanceTest extends IntegrationTestCase {
 
 	/**
+	 * {@inheritDoc}
+	 */
+	public static function setUpBeforeClass() {
+		parent::setUpBeforeClass();
+
+		// there must be any data in the table to get a result at all, but it's fine to only add the dummy data once
+		static::persistDummyGeoPostalCodes(1);
+	}
+
+	protected function cleanDatabaseBeforeTest() {
+		// don't clean
+	}
+
+	/**
 	 * @dataProvider dataGeoDistance
 	 */
 	public function testGeoDistance($latOrigin, $lngOrigin, $latDestination, $lngDestination, $expectedDistance) {
-		// there must be any data in the table to get a result at all
-		static::persistDummyGeoPostalCodes(1);
-
 		$qb = static::getRepo()->createQueryBuilder('poi')
 			->select('GEO_DISTANCE(:latOrigin, :lngOrigin, :latDestination, :lngDestination)')
 			->setParameter('latOrigin', $latOrigin)
@@ -36,6 +47,8 @@ class GeoDistanceTest extends IntegrationTestCase {
 		return array(
 			array(52.392759, 13.065135, 52.392759, 13.065135, 0),
 			array(52.392759, 13.065135, 52.525011, 13.369438, 25.32498093345365),
+			array(-43.5131367, 172.5990772, -43.8951617, 171.7203311, 82.42610380554926),
+			array(-0.1865943, -78.4305382, 0.3516889, -78.1234253, 68.91091929958483),
 		);
 	}
 
