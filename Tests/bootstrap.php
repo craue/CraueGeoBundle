de080447ac02c02103bac00bcd89b2689831f092
Christian Raue <christian.raue@gmail.com>
2015-09-21 11:03:21 +0200

    added a comment to explain why some test code is not useless

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -4,4 +4,5 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $loader = require __DIR__.'/../vendor/autoload.php';
 
+// needed for Symfony < 2.7
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
