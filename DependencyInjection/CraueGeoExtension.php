9fdcf67adf9fd29ab2246e8143b789c29b529bb3
Christian Raue <christian.raue@gmail.com>
2015-09-30 00:02:31 +0200

    register Doctrine functions automatically while providing a way to set custom names

---
+++ DependencyInjection/CraueGeoExtension.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Craue\GeoBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+
+/**
+ * Registration of the extension via DI.
+ *
+ * @author Christian Raue <christian.raue@gmail.com>
+ * @copyright 2011-2015 Christian Raue
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ */
+class CraueGeoExtension extends Extension implements PrependExtensionInterface {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function load(array $config, ContainerBuilder $container) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function prepend(ContainerBuilder $container) {
+		$config = $this->processConfiguration(new Configuration(), $container->getExtensionConfig($this->getAlias()));
+
+		$container->prependExtensionConfig('doctrine', array(
+			'orm' => array(
+				'dql' => array(
+					'numeric_functions' => array(
+						$config['functions']['geo_distance'] => 'Craue\GeoBundle\Doctrine\Query\Mysql\GeoDistance',
+						$config['functions']['geo_distance_by_postal_code'] => 'Craue\GeoBundle\Doctrine\Query\Mysql\GeoDistanceByPostalCode',
+					),
+				),
+			),
+		));
+	}
+
+}
---
+++ Tests/Doctrine/Query/Mysql/CustomFunctionNameTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Craue\GeoBundle\Tests\Doctrine\Query\Mysql;
+
+use Craue\GeoBundle\Tests\IntegrationTestCase;
+
+/**
+ * @group integration
+ *
+ * @author Christian Raue <christian.raue@gmail.com>
+ * @copyright 2011-2015 Christian Raue
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ */
+class CustomFunctionNameTest extends IntegrationTestCase {
+
+	/**
+	 * Ensure that custom function names will be used to register the corresponding functions.
+	 */
+	public function testCustomFunctionName() {
+		$this->initClient(array('environment' => 'customFunctionName', 'config' => 'config_customFunctionName.yml'));
+		$this->assertSame('Craue\GeoBundle\Doctrine\Query\Mysql\GeoDistance',
+				$this->getEntityManager()->getConfiguration()->getCustomNumericFunction('CRAUE_GEO_DISTANCE'));
+		$this->assertSame('Craue\GeoBundle\Doctrine\Query\Mysql\GeoDistanceByPostalCode',
+				$this->getEntityManager()->getConfiguration()->getCustomNumericFunction('CRAUE_GEO_DISTANCE_BY_POSTAL_CODE'));
+	}
+
+	/**
+	 * Ensure that a user-defined function will override the bundle-defined default one to preserve BC.
+	 */
+	public function testOverrideFunction() {
+		$this->initClient(array('environment' => 'overrideFunction', 'config' => 'config_overrideFunction.yml'));
+		$this->assertSame('Craue\GeoBundle\Doctrine\Query\Mysql\GeoDistance',
+				$this->getEntityManager()->getConfiguration()->getCustomNumericFunction('MY_GEO_DISTANCE'));
+	}
+
+}
