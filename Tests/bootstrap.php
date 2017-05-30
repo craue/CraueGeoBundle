88e32566a31adaf30442058cbd7f78622646c0d8
Christian Raue <christian.raue@gmail.com>
2017-05-30 12:32:41 +0200

    dropped support for Symfony 2.3

--- Tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Doctrine\Common\Annotations\AnnotationRegistry;
-
-$loader = require __DIR__.'/../vendor/autoload.php';
-
-// needed for Symfony < 2.7
-AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
