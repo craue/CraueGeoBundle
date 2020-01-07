426079b7152d3e3dc1d8cc27d64830609cbaedab
Christian Raue <christian.raue@gmail.com>
2020-01-07 11:06:28 +0100

    updated copyright year range

--- Tests/Doctrine/Fixtures/FixtureTest.php
+++ Tests/Doctrine/Fixtures/FixtureTest.php
@@ -13,7 +13,7 @@ use Symfony\Component\Console\Tester\CommandTester;
  * @group integration
  *
  * @author Christian Raue <christian.raue@gmail.com>
- * @copyright 2011-2019 Christian Raue
+ * @copyright 2011-2020 Christian Raue
  * @license http://opensource.org/licenses/mit-license.php MIT License
  */
 class FixtureTest extends IntegrationTestCase {
