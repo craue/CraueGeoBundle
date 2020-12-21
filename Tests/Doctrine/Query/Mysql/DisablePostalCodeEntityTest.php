a77e543c14bd5c63596bbabd21115985f9e3969f
Christian Raue <christian.raue@gmail.com>
2020-12-21 11:17:14 +0100

    bumped dependencies
    - added support for PHP 8.0
    - dropped support for PHP 7.0, 7.1, 7.2

--- Tests/Doctrine/Query/Mysql/DisablePostalCodeEntityTest.php
+++ Tests/Doctrine/Query/Mysql/DisablePostalCodeEntityTest.php
@@ -3,6 +3,7 @@
 namespace Craue\GeoBundle\Tests\Doctrine\Query\Mysql;
 
 use Craue\GeoBundle\Tests\IntegrationTestCase;
+use Doctrine\ORM\Mapping\MappingException;
 
 /**
  * @group integration
@@ -17,17 +18,17 @@ class DisablePostalCodeEntityTest extends IntegrationTestCase {
 	 * Ensure that the GeoPostalCode class is not registered as a Doctrine entity.
 	 *
 	 * @dataProvider dataDisablePostalCodeEntity
-	 *
-	 * @expectedException \Doctrine\ORM\Mapping\MappingException
-	 * @expectedExceptionMessage Class "Craue\GeoBundle\Entity\GeoPostalCode"
-	 * @expectedExceptionMessage is not a valid entity or mapped super class.
-	 *
-	 * Note: Doctrine ORM 2.3.0 would throw an exception with message
-	 *   Class "Craue\GeoBundle\Entity\GeoPostalCode" sub class of "" is not a valid entity or mapped super class.
-	 * here, so just verify the relevant parts of the message. This has been fixed in ORM 2.3.1.
 	 */
 	public function testDisablePostalCodeEntity($platform, $config, $requiredExtension) {
 		$this->initClient($requiredExtension, ['environment' => 'disablePostalCodeEntity_' . $platform, 'config' => $config]);
+
+		$this->expectException(MappingException::class);
+		/*
+		 * Doctrine ORM 2.3.0 would throw an exception with message
+		 *   Class "Craue\GeoBundle\Entity\GeoPostalCode" sub class of "" is not a valid entity or mapped super class.
+		 * here, so just verify the relevant parts of the message. This has been fixed in ORM 2.3.1.
+		 */
+		$this->expectExceptionMessageMatches(sprintf('/^%s.*%s$/s', preg_quote('Class "Craue\GeoBundle\Entity\GeoPostalCode"'), preg_quote('is not a valid entity or mapped super class.')));
 
 		$this->getRepo();
 	}
--- Tests/Doctrine/Query/Mysql/FlavorTest.php
+++ Tests/Doctrine/Query/Mysql/FlavorTest.php
@@ -3,6 +3,8 @@
 namespace Craue\GeoBundle\Tests\Doctrine\Query\Mysql;
 
 use Craue\GeoBundle\Tests\IntegrationTestCase;
+use Doctrine\ORM\Query\QueryException;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 
 /**
  * @group integration
@@ -15,34 +17,34 @@ class FlavorTest extends IntegrationTestCase {
 
 	/**
 	 * Ensure that only valid values can be used for the flavor.
-	 *
-	 * @expectedException \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
-	 * @expectedExceptionMessage The value "invalid" is not allowed for path "craue_geo.flavor". Permissible values: "none", "mysql", "postgresql"
 	 */
 	public function testFlavorInvalid() {
+		$this->expectException(InvalidConfigurationException::class);
+		$this->expectExceptionMessage('The value "invalid" is not allowed for path "craue_geo.flavor". Permissible values: "none", "mysql", "postgresql"');
+
 		$this->initClient(null, ['environment' => 'invalidFlavor', 'config' => ['config.yml', 'config_flavor_invalid.yml']]);
 	}
 
 	/**
 	 * Ensure that function GEO_DISTANCE is not registered when using flavor 'none'.
-	 *
-	 * @expectedException \Doctrine\ORM\Query\QueryException
-	 * @expectedExceptionMessage Error: Expected known function, got 'GEO_DISTANCE'
 	 */
 	public function testFlavorNone_geoDistance() {
 		$this->initClient(null, ['environment' => 'flavorNone', 'config' => ['config.yml', 'config_flavor_none.yml']]);
+
+		$this->expectException(QueryException::class);
+		$this->expectExceptionMessage("Error: Expected known function, got 'GEO_DISTANCE'");
 
 		$this->getPoisPerGeoDistance(52.1, 13.1, 1);
 	}
 
 	/**
 	 * Ensure that function GEO_DISTANCE_BY_POSTAL_CODE is not registered when using flavor 'none'.
-	 *
-	 * @expectedException \Doctrine\ORM\Query\QueryException
-	 * @expectedExceptionMessage Error: Expected known function, got 'GEO_DISTANCE_BY_POSTAL_CODE'
 	 */
 	public function testFlavorNone_geoDistanceByPostalCode() {
 		$this->initClient(null, ['environment' => 'flavorNone', 'config' => ['config.yml', 'config_flavor_none.yml']]);
+
+		$this->expectException(QueryException::class);
+		$this->expectExceptionMessage("Error: Expected known function, got 'GEO_DISTANCE_BY_POSTAL_CODE'");
 
 		$this->getPoisPerGeoDistanceByPostalCode('DE', '123');
 	}
